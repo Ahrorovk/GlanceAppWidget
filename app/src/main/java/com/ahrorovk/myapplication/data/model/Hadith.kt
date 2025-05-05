f73cfe4e7153163632825dc797fe53ada46ae3e4
AhrorovK <akhrorovk5758@gmail.com>
2025-05-05 20:14:39 +0300

    update apk and hadith default lang change

--- app/src/main/java/com/ahrorovk/myapplication/data/model/Hadith.kt
+++ app/src/main/java/com/ahrorovk/myapplication/data/model/Hadith.kt
@@ -1,6 +1,6 @@
 package com.ahrorovk.myapplication.data.model
 
 data class Hadith(
-    val hadith: String = "Ежедневные хадисы",
-    val author: String = "[Бухари]"
+    val hadith: String = "Daily Hadith",
+    val author: String = "[Sahih-Bukhari]"
 )
